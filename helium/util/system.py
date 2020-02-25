dc1ea33fd7c8f22ab011052be29f49d1f1aebc0b
Michael Herrmann <michael@herrmann.io>
2020-02-25 16:17:37 +0100

    Remove unused functions

--- helium/util/system.py
+++ helium/util/system.py
@@ -1,9 +1,6 @@
 """
 Gives information about the current operating system.
 """
-from os import popen
-
-import os
 import sys
 
 def is_windows():
@@ -22,23 +19,3 @@ def get_canonical_os_name():
 		return 'mac'
 	elif is_linux():
 		return 'linux'
-
-def is_32_bit():
-	"""
-	Determines whether the current OS is 32- or 64 bit. Note that this may be
-	different from the bitness of the Python interpreter or the CPU. Eg.
-	is_32_bit() returns True on a 32 bit Windows running on a 64 bit processor.
-	"""
-	if is_windows():
-		return 'PROGRAMFILES(X86)' not in os.environ
-	assert is_linux() or is_mac()
-	return _uname_m() in ('i386', 'i686')
-
-def _uname_m():
-	return popen('uname -m').read().strip()
-
-def is_64_bit():
-	if is_windows():
-		return 'PROGRAMFILES(X86)' in os.environ
-	assert is_linux() or is_mac()
-	return _uname_m() in ('x86_64', 'ia64', 'amd64')
