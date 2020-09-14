8d39975dc8d7f70a185ee6418146ff78486d76bf
Michael Herrmann <michael@herrmann.io>
2020-09-14 14:10:24 +0200

    Set version number for release 3.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'helium',
-	version = '3.0.5-SNAPSHOT',
+	version = '3.0.5',
 	author = 'Michael Herrmann',
 	author_email = 'michael+removethisifyouarehuman@herrmann.io',
 	description = 'Lighter browser automation based on Selenium.',
