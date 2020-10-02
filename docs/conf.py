e926b290e709dd2e82ac593283c3874bcf93ce19
IgnisDa <ignisda2002@gmail.com>
2020-10-02 15:19:50 +0530

    Remove .gitignore artifacts that are not required

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@ release = '3.0.6-SNAPSHOT'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx_rtd_theme', 'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,3 +42,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# autodoc_member_order = 'bysource'
