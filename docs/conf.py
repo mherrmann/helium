67e35bdabbf2978991ff4f1bbaf605e1f03c0b2f
Michael Herrmann <michael@herrmann.io>
2021-03-19 07:57:13 +0100

    Bump version for next development iteration

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ copyright = '%s, Michael Herrmann' % date.today().year
 author = 'Michael Herrmann'
 
 # Also update ../setup.py when you change this:
-release = '3.0.6'
+release = '3.0.7-SNAPSHOT'
 
 
 # -- General configuration ---------------------------------------------------
