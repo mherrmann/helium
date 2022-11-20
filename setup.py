6a363a80c1aeeaa01cb9041c71f655b52f04eb8d
Michael Herrmann <michael@herrmann.io>
2022-11-20 16:56:09 +0900

    Add 3.9 and 3.10 to supported Py versions

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
 		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
 		'Operating System :: Microsoft :: Windows',
 		'Operating System :: POSIX :: Linux',
 		'Operating System :: MacOS :: MacOS X'
