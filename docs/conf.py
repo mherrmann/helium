60d661130b18d1bc0dc41e338e5c18c54f847851
Michael Herrmann <michael@herrmann.io>
2023-09-27 14:56:51 +0200

    Set version number for release 3.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ copyright = '%s, Michael Herrmann' % date.today().year
 author = 'Michael Herrmann'
 
 # Also update ../setup.py when you change this:
-release = '3.2.5'
+release = '3.2.6'
 
 
 # -- General configuration ---------------------------------------------------
