10347f3fbb961cfb4d44bcac7e2d84e9294a756b
Tim Powell <tim.powell@veeva.com>
2023-10-30 17:48:13 -0700

    switch to selenium 4.9.x, sync up with rebase from main. Change version number to 4.0.0 to indicate major revision (of selenium at least) Mark as supporting python 3.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name = 'helium',
 	# Also update docs/conf.py when you change this:
-	version = '3.2.6',
+	version = '4.0.0',
 	author = 'Michael Herrmann',
 	author_email = 'michael+removethisifyouarehuman@herrmann.io',
 	description = 'Lighter browser automation based on Selenium.',
@@ -13,7 +13,8 @@ setup(
 	packages = find_packages(exclude=['tests', 'tests.*']),
 	install_requires = [
 		# Also update requirements/base.txt when you make changes here.
-		'selenium==4.9.0'
+		'selenium>=4.9.0,<4.10',
+		'webdriver_manager'
 	],
 	package_data = {
 		'helium._impl': ['webdrivers/**/*']
@@ -32,6 +33,7 @@ setup(
 		'Programming Language :: Python :: 3.8',
 		'Programming Language :: Python :: 3.9',
 		'Programming Language :: Python :: 3.10',
+		'Programming Language :: Python :: 3.11',
 		'Operating System :: Microsoft :: Windows',
 		'Operating System :: POSIX :: Linux',
 		'Operating System :: MacOS :: MacOS X'
