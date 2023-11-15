b170b525c84ab47351bd44d30cd07eed7eaa83fd
Michael Herrmann <michael@herrmann.io>
2023-11-15 15:20:58 +0800

    Attempt to fix HTML docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,16 +32,3 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 autodoc_member_order = 'bysource'
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
