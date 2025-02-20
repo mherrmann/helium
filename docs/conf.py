59929ede52a0faab08711c0b80d26388e29c0338
Michael Herrmann <michael@herrmann.io>
2025-02-20 17:04:15 +0100

    Bump version to 5.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ copyright = '%s, Michael Herrmann' % date.today().year
 author = 'Michael Herrmann'
 
 # Also update ../setup.py when you change this:
-release = '5.1.0'
+release = '5.1.1'
 
 
 # -- General configuration ---------------------------------------------------
