6be4a8003bcbca6a71676c401d21408a229a2a7d
fujitsubo <110240244+masafumi-kiyokawa@users.noreply.github.com>
2025-04-28 13:35:46 +0900

    Fix docs build errors (#133)

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,9 @@ release = '5.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinx_rtd_theme']
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
