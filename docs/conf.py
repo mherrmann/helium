1cb4f828626f771da21dec2ebb641889b73d26a4
Michael Herrmann <michael@herrmann.io>
2026-02-04 17:23:22 +0100

    Bump version to 7.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ copyright = '%s, Michael Herrmann' % date.today().year
 author = 'Michael Herrmann'
 
 # Also update ../setup.py when you change this:
-release = '6.0.0'
+release = '7.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name = 'helium',
 	# Also update docs/conf.py when you change this:
-	version = '6.0.0',
+	version = '7.0.0',
 	author = 'Michael Herrmann',
 	author_email = 'michael+removethisifyouarehuman@herrmann.io',
 	description = 'Lighter browser automation based on Selenium.',
